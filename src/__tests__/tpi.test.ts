b32d22ae39c6fdf12c1e80e4312d9a54caaf08da
Mike Farr <mike@noneleft.ca>
2023-01-08 15:14:18 -0500

    Fix test names

--- src/__tests__/tpi.test.ts
+++ src/__tests__/tpi.test.ts
@@ -1,42 +1,42 @@
 import tpi from "../tpi";
 
-test("ParseCommand with valid input returns command", () => {
+test("parseCommand with valid input returns command", () => {
   const command = tpi.parseCommand("510AB");
 
   expect(command.command).toBe("510");
 });
 
-test("ParseCommand with invalid input returns empty command", () => {
+test("parseCommand with invalid input returns empty command", () => {
   const command = tpi.parseCommand("510");
 
   expect(command.command).toBe("");
 });
 
-test("ParseChecksum with valid input returns checksum", () => {
+test("parseChecksum with valid input returns checksum", () => {
   const checksum = tpi.parseChecksum("510AB");
 
   expect(checksum.value).toBe("AB");
 });
 
-test("ParseChecksum with invalid input returns empty checksum", () => {
+test("parseChecksum with invalid input returns empty checksum", () => {
   const checksum = tpi.parseChecksum("510");
 
   expect(checksum.value).toBe("");
 });
 
-test("ParseData with valid input returns data", () => {
+test("parseData with valid input returns data", () => {
   const data = tpi.parseData("005123456AB");
 
   expect(data.value).toBe("123456");
 });
 
-test("ParseData with no data returns empty data object", () => {
+test("parseData with no data returns empty data object", () => {
   const data = tpi.parseData("510AB");
 
   expect(data.value).toBe("");
 });
 
-test("ParseData with invalid input returns empty data object", () => {
+test("parseData with invalid input returns empty data object", () => {
   const data = tpi.parseData("510");
 
   expect(data.value).toBe("");
