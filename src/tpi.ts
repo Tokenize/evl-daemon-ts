3673f52ce09abc31f955b45efd732d6c634c8666
Mike Farr <mike@noneleft.ca>
2023-09-29 07:16:19 -0400

    WIP: Add TPI utility functions

---
+++ src/tpi.ts
@@ -0,0 +1,15 @@
+import { Command } from "./types";
+
+export const COMMAND_LENGTH = 3;
+
+export const CHECKSUM_LENGTH = 2;
+
+export const parseCommand = (input: string): Command => {
+  let command = "";
+
+  if (input.length >= COMMAND_LENGTH + CHECKSUM_LENGTH) {
+    command = input.substring(0, COMMAND_LENGTH);
+  }
+
+  return { command: command };
+};
