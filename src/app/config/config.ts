5cbfcec11d089702f06a77feb8067e1e78fee18c
Mike Farr <mike@noneleft.ca>
2024-01-10 07:11:00 -0500

    Use EvlConfig object as default export

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,8 +1,10 @@
 import convict from "convict";
 import { EvlConfig, Schema } from "./schema";
 
-export const config = convict<EvlConfig>(Schema);
+const config = convict<EvlConfig>(Schema);
 
 config.loadFile("./evl-daemon.json");
 
 config.validate();
+
+export default config.getProperties();
