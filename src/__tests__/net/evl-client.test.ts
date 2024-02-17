83bfa83dbdac5a480c1895465c0f77095fa44d75
Mike Farr <mike@noneleft.ca>
2024-02-17 17:06:23 -0500

    Add a few tests for basic functions

--- src/__tests__/net/evl-client.test.ts
+++ src/__tests__/net/evl-client.test.ts
@@ -1,11 +1,76 @@
-test("connect attempt to connect if not already connected");
+import { EvlSocketConnection } from "../../app/net/evl-connection";
+import { EvlClient } from "../../app/net/evl-client";
 
-test("connect should not attempt to connect if already connected");
+let evlClient: EvlClient;
+let connectMock: jest.SpyInstance;
 
-test("send should throw error if not connected");
+beforeEach(() => {
+  evlClient = new EvlClient(
+    new EvlSocketConnection("localhost", 4025),
+    "password",
+  );
 
-test("send should send given data if connected");
+  connectMock = jest
+    .spyOn(EvlSocketConnection.prototype, "connect")
+    .mockImplementation(() => {});
 
-test("should emit disconnect event when disconnected");
+  jest.mock("../../app/net/evl-connection").resetAllMocks();
+});
 
-test("should emit ");
+describe("connect", () => {
+  test("attempts to connect if not already connected", () => {
+    const getConnectedMock = jest
+      .spyOn(EvlSocketConnection.prototype, "connected", "get")
+      .mockImplementation(() => false);
+
+    evlClient.connect();
+
+    expect(getConnectedMock).toHaveBeenCalled();
+
+    expect(connectMock).toHaveBeenCalled();
+  });
+
+  test("doesn't attempt to connect if already connected", () => {
+    const getConnectedMock = jest
+      .spyOn(EvlSocketConnection.prototype, "connected", "get")
+      .mockImplementation(() => true);
+
+    evlClient.connect();
+
+    expect(getConnectedMock).toHaveBeenCalled();
+
+    expect(connectMock).toHaveBeenCalledTimes(0);
+  });
+});
+
+describe("send", () => {
+  test("should throw error if not connected", () => {
+    const getConnectedMock = jest
+      .spyOn(EvlSocketConnection.prototype, "connected", "get")
+      .mockImplementation(() => false);
+
+    expect(() => {
+      evlClient.send("test");
+    }).toThrow();
+
+    expect(getConnectedMock).toHaveBeenCalled();
+  });
+
+  test("should send given data if connected", () => {
+    jest
+      .spyOn(EvlSocketConnection.prototype, "connected", "get")
+      .mockImplementation(() => true);
+
+    const sendMock = jest
+      .spyOn(EvlSocketConnection.prototype, "send")
+      .mockImplementation(() => {});
+
+    evlClient.send("data");
+
+    expect(sendMock).toHaveBeenLastCalledWith<string[]>("data");
+  });
+});
+
+test("should emit disconnect event when disconnected", () => {});
+
+test("should emit data event when data is received", () => {});
