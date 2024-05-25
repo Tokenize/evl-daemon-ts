5b8104261bf765f57560bd59665d0bad9fbe7d3b
Mike Farr <mike@noneleft.ca>
2024-05-25 07:43:44 -0400

    Use winston for logging implementation

---
+++ src/__tests__/logging/logger.test.ts
@@ -0,0 +1,185 @@
+import { LogDestination, LogDestinationType, LogLevel, Logger } from "../../app/logging/logger";
+
+const mockLog = jest.fn();
+const mockAdd = jest.fn();
+const mockCreateLogger = jest.fn().mockReturnValue({
+  log: mockLog,
+  add: mockAdd,
+});
+
+const consoleDestination: LogDestination = {
+  type: "console",
+  level: LogLevel.Info,
+  name: "console",
+  format: "simple",
+  settings: {},
+};
+
+const consoleDestination2: LogDestination = {
+  type: "console",
+  level: LogLevel.Info,
+  name: "console2",
+  format: "simple",
+  settings: {},
+};
+
+jest.mock("winston", () => {
+  return {
+    createLogger: (): unknown => mockCreateLogger(),
+    format: {
+      simple: jest.fn(),
+    },
+    transports: {
+      Console: jest.fn(),
+      File: jest.fn(),
+    },
+  };
+});
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+describe("addDestinations", () => {
+  test("should add a transport for each destination", () => {
+    const logger = new Logger();
+    const destinations: LogDestination[] = [consoleDestination];
+
+    logger.addDestinations(destinations);
+
+    expect(logger.count).toBe(1);
+  });
+
+  test("should not add a transport for an invalid destination", () => {
+    const logger = new Logger();
+    const destinations: LogDestination[] = [
+      {
+        type: "invalid" as unknown as LogDestinationType,
+        level: LogLevel.Info,
+        name: "invalid",
+        format: "simple",
+        settings: {},
+      },
+    ];
+
+    logger.addDestinations(destinations);
+
+    expect(logger.count).toBe(0);
+  });
+
+  test("should add a transport for each valid destination", () => {
+    const logger = new Logger();
+    const destinations: LogDestination[] = [consoleDestination, consoleDestination2];
+
+    logger.addDestinations(destinations);
+
+    expect(logger.count).toBe(2);
+  });
+
+  test("should add a transport for each valid destination and ignore invalid destinations", () => {
+    const logger = new Logger();
+    const destinations: LogDestination[] = [
+      consoleDestination,
+      {
+        type: "invalid" as unknown as LogDestinationType,
+        level: LogLevel.Info,
+        name: "invalid",
+        format: "simple",
+        settings: {},
+      },
+      consoleDestination2,
+    ];
+
+    logger.addDestinations(destinations);
+
+    expect(logger.count).toBe(2);
+  });
+
+  test("should throw an error if provided level is invalid", () => {
+    const logger = new Logger();
+    const destinations: LogDestination[] = [
+      {
+        ...consoleDestination,
+        level: "invalid" as unknown as LogLevel,
+      },
+    ];
+
+    expect(() => {
+      logger.addDestinations(destinations);
+    }).toThrow();
+  });
+
+  test("should throw an error if destination name is missing", () => {
+    const logger = new Logger();
+    const destinations: LogDestination[] = [
+      {
+        type: "console",
+        level: LogLevel.Info,
+        format: "simple",
+        settings: {},
+      } as unknown as LogDestination,
+    ];
+
+    expect(() => {
+      logger.addDestinations(destinations);
+    }).toThrow();
+  });
+
+  test("should throw an error if destination format is missing", () => {
+    const logger = new Logger();
+    const destinations: LogDestination[] = [
+      {
+        type: "console",
+        level: LogLevel.Info,
+        name: "console",
+        settings: {},
+      } as unknown as LogDestination,
+    ];
+
+    expect(() => {
+      logger.addDestinations(destinations);
+    }).toThrow();
+  });
+
+  test("should throw an error if destination is invalid", () => {
+    const logger = new Logger();
+    const destinations: LogDestination[] = [{} as unknown as LogDestination];
+
+    expect(() => {
+      logger.addDestinations(destinations);
+    }).toThrow();
+  });
+});
+
+describe("logging", () => {
+  test("log should log a message to each destination", () => {
+    const logger = new Logger();
+    const destinations: LogDestination[] = [consoleDestination];
+
+    logger.addDestinations(destinations);
+
+    logger.log(LogLevel.Info, "Test message");
+
+    expect(mockLog).toHaveBeenCalledTimes(1);
+    expect(mockLog).toHaveBeenCalledWith("info", "Test message");
+  });
+
+  it.each([
+    ["logInfo", "info", "Info!"],
+    ["logWarning", "warn", "Warning!"],
+    ["logError", "error", "Error!"],
+    ["logDebug", "debug", "Debug!"],
+    ["logTrace", "silly", "Trace!"],
+  ])("%s should log a message at %s level", (logFn, level, message) => {
+    const logger = new Logger();
+    const destinations: LogDestination[] = [consoleDestination];
+
+    logger.addDestinations(destinations);
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
+    (logger as any)[logFn](message);
+
+    expect(mockLog).toHaveBeenCalledTimes(1);
+    expect(mockLog).toHaveBeenCalledWith(level, message);
+  });
+});
--- src/app/logging/logger.ts
+++ src/app/logging/logger.ts
@@ -17,34 +17,65 @@ export type LogDestination = {
   name: string;
   level: LogLevel;
   format: string;
-  settings: Record<string, string | number>;
+  settings: Record<string, string | number> | null;
 };
 
 export class Logger {
   private _logger: winston.Logger;
 
+  private _destinations: Map<LogDestination, winston.transport> = new Map();
+
+  private readonly _levelMap: Map<LogLevel, string> = new Map([
+    [LogLevel.Error, "error"],
+    [LogLevel.Warning, "warn"],
+    [LogLevel.Info, "info"],
+    [LogLevel.Debug, "debug"],
+    [LogLevel.Trace, "silly"],
+  ]);
+
   constructor(level: LogLevel = LogLevel.Info) {
     this._logger = winston.createLogger({
-      level: this.translateLogLevel(level),
+      level: this.mapLogLevel(level),
       format: winston.format.simple(),
     });
   }
 
+  public get count(): number {
+    return this._destinations.size;
+  }
+
   public addDestinations(destinations: LogDestination[]): void {
     destinations.forEach((destination) => {
+      this.validateDestination(destination);
+
       const transport = this.createTransport(destination);
 
       if (transport) {
+        this._destinations.set(destination, transport);
         this._logger.add(transport);
       }
     });
+  }
+
+  private validateDestination(destination: LogDestination): void {
+    if (!destination.name) {
+      throw new Error("Destination name is required");
+    }
+
+    if (!destination.format) {
+      throw new Error("Destination format is required");
+    }
+
+    if (this.mapLogLevel(destination.level) == null) {
+      throw new Error("Invalid log level");
+    }
   }
 
   private createTransport(destination: LogDestination): winston.transport | null {
     switch (destination.type) {
       case "console":
         return new winston.transports.Console({
-          level: this.translateLogLevel(destination.level),
+          level: this.mapLogLevel(destination.level),
           format: winston.format.simple(),
         });
       default:
@@ -52,21 +83,8 @@ export class Logger {
     }
   }
 
-  private translateLogLevel(level: LogLevel): string {
-    switch (level) {
-      case LogLevel.Error:
-        return "error";
-      case LogLevel.Warning:
-        return "warn";
-      case LogLevel.Info:
-        return "info";
-      case LogLevel.Debug:
-        return "debug";
-      case LogLevel.Trace:
-        return "silly";
-      default:
-        return "info";
-    }
+  private mapLogLevel(level: LogLevel): string | undefined {
+    return this._levelMap.get(level);
   }
 
   public logError(message: string, ...params: LogMessageParameter[]): void {
@@ -90,7 +108,7 @@ export class Logger {
   }
 
   public log(level: LogLevel, message: string, ...params: LogMessageParameter[]): void {
-    const translatedLevel = this.translateLogLevel(level);
+    const translatedLevel = this.mapLogLevel(level) ?? "info";
 
     this._logger.log(translatedLevel, message, ...params);
   }
