dcac03b2421f92b078db1d7262159a2f61f36e5e
Mike Farr <mike@noneleft.ca>
2024-06-05 06:56:37 -0400

    Throw informative error when config file missing (#116)
    Throw error when config file can't be opened

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -10,6 +10,8 @@ config.set("priorities", CommandPriorities);
 
 if (existsSync("./evl-daemon.json")) {
   config.loadFile("./evl-daemon.json");
+} else {
+  throw new Error("Configuration file not found.");
 }
 
 config.validate();
